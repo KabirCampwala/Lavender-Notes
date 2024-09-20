d9c005841b440b2e4611e44fbcd9558e01040258
Kabir <kabircampwala3@gmail.com>
2024-09-20 20:34:35 +0530

    Completed Landing Page

--- app/(landing)/_components/heroes.tsx
+++ app/(landing)/_components/heroes.tsx
@@ -2,8 +2,37 @@ import Image from "next/Image";
 
 export const Heroes = () => {
     return (
-        <div>
-            Heroes
+        <div className="flex flex-col items-center justify-center max-w-5xl">
+            <div className="flex items-center">
+                <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+                    <Image
+                        src="/document-light.png"
+                        fill
+                        className="object-contain dark:hidden"
+                        alt="Document"
+                    />
+                    <Image
+                        src="/reading-dark.png"
+                        fill
+                        className="object-contain hidden dark:block"
+                        alt="Reading"
+                    />
+                </div>
+                <div className="relative h-[400px] w-[400px] hidden md:block">
+                    <Image
+                        src="/typing-light.png"
+                        fill
+                        className="object-contain dark:hidden"
+                        alt="Reading"
+                    />
+                    <Image
+                        src="/typing-dark.png"
+                        fill
+                        className="object-contain hidden dark:block"
+                        alt="Reading"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -37,11 +38,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+        storageKey="lavender-theme-2"
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
